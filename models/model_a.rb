d84cc662148d3ed518107411c9751f93754d0e76
Amanda McGuire <aruwart001@gmail.com>
2021-04-02 09:22:16 -0600

    Add dinosaur facts to file

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "name" => "Allosaurus",
+  "period" => "Late Jurrasic",
+  "weight in tons" => 2,
+  "height in ft" => 35,
+  "location" => ["Utah", "Colorado", "Wyoming"],
+  "utah state fossil" => true,
+  "carnivore" => true,
+  "features" => ["short neck", "elongated skull", "large tail and hind limbs"]
+}
