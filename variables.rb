5cd1ebb858d4cee72b9173aa6f910e305d377884
Amanda McGuire <aruwart001@gmail.com>
2021-04-02 08:56:05 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "The Secret Life of Walter Mitty"
+is_entertaining = true
+movie = "Top Gun"
+num_4 = 4
+num_6 = 6
+product = num_6 * num_4
+favorite_foods = ["pizza", "tacos", "fried rice"]
+favorite_foods.shift
+puts movie.length
