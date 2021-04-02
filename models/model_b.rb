b59c02c9a9a0e892d776692741515f1a3ef2733d
Amanda McGuire <aruwart001@gmail.com>
2021-04-02 09:46:26 -0600

    Add developer facts to file

---
+++ models/model_b.rb
@@ -0,0 +1,9 @@
+developer = {
+  "name" => "backend",
+  "languages" => ["Java", "Ruby", "C++", "Python"],
+  "website components" => ["servers," "applications", "databases"],
+  "style webpage" => false,
+  "average salary" => 100000,
+  "hiring" = true,
+  "average hours per week" => 45  
+}
